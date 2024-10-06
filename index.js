c5c4137ed93dfeeff799852d984b46a6c6607465
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-06 16:44:34 +0600

    spinner funtioanality added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const showCategoryBtn = (array) => {
         categoryBtnDiv.innerHTML +=
             `
         <div>
-            <button class="btn w-full h-[70px] lg:h-24 rounded-2xl flex items-center justify-center space-x-2 common-category" onclick=fetchAllPets(this,'${category}','${id}')  id = "${id}">
+            <button class="btn w-full h-[70px] lg:h-24 rounded-2xl flex items-center justify-center space-x-2 common-category" onclick=fetchAllPets(this,'${category}')>
                 <div><img src="${category_icon} " class="w-14 h-14" alt=""></div>
                 <div class=" text-base md:text-xl lg:text-2xl font-bold text-primary">${category} </div>
             </button>
@@ -28,10 +28,10 @@ const showCategoryBtn = (array) => {
 // active category btn
 const activeBtn = (btnElement) => {
     allBtn = document.getElementsByClassName('common-category');
-    for(let item of allBtn){
+    for (let item of allBtn) {
         item.classList.remove('active');
         item.classList.add('rounded-2xl');
-        
+
     }
     btnElement.classList.remove('rounded-2xl')
     btnElement.classList.add('active')
@@ -41,7 +41,8 @@ const activeBtn = (btnElement) => {
 
 
 // animal card section
-const fetchAllPets = async (btnElement, categoryName, id) => {
+const fetchAllPets = async (btnElement, categoryName) => {
+    document.getElementById('spinner').classList.add('hidden');
     if (categoryName) {
         const res = await fetch(`https://openapi.programming-hero.com/api/peddy/category/${categoryName}`)
         const data = await res.json()
@@ -56,59 +57,76 @@ const fetchAllPets = async (btnElement, categoryName, id) => {
     }
 
 }
+
 fetchAllPets()
 
+// loading by spinner
+// const loading = () =>{
+//     document.getElementById('spinner').classList.remove('hidden');
+//     document.getElementById('animal-card-section');
+//     setTimeout(()=>{
+//         fetchAllPets()       
+
+//     },5000)
+// }
+// loading();
 const showAllPets = (array) => {
     const cardParentDiv = document.getElementById('animal-card-parent-div')
     cardParentDiv.innerHTML = '';
-    if (array.length == 0) {
-        document.getElementById('empty-section').classList.remove('hidden');
+    document.getElementById('spinner').classList.remove('hidden');
+    document.getElementById('animal-card-section').classList.add('hidden');
+    setTimeout(() => {
+        document.getElementById('spinner').classList.add('hidden');
+        document.getElementById('animal-card-section').classList.remove('hidden');
+        if (array.length == 0) {
+            document.getElementById('empty-section').classList.remove('hidden');
 
-    } else {
-        document.getElementById('empty-section').classList.add('hidden');
-    }
-    array.forEach((item) => {
-        const { breed, category, date_of_birth, gender, image, petId, pet_details, pet_name, price, vaccinated_status } = item
-        cardParentDiv.innerHTML +=
+        } else {
+            document.getElementById('empty-section').classList.add('hidden');
+        }
+        array.forEach((item) => {
+            const { breed, category, date_of_birth, gender, image, petId, pet_details, pet_name, price, vaccinated_status } = item
+            cardParentDiv.innerHTML +=
+                `
+            <div class="p-5 rounded-xl border border-solid border-[primary]/[0.1]">
+                <div class="mb-6"><img src="${image} " class="w-full rounded-lg h-[300px] md:h-[330px] lg:h-[360px] object-cover" alt=""></div>
+                <h1 class="text-base md:text-lg lg:text-xl font-bold text-primary mb-4">${pet_name} </h1>
+                <div class="text-sm md:text-sm lg:text-base font-normal text-primary/[0.7] mb-4 space-y-1">
+                    <p class="flex items-center">
+                        <span class="mr-2"><img src="images/icon/square.png" class="w-5 h-5" alt=""></span>
+                        <span>Breed: ${breed ? breed : `Not found`} </span>
+                    </p>
+                    <p class="flex items-center">
+                        <span class="mr-2"><img src="images/icon/calender.png" class="w-5 h-5"  alt=""></span>
+                        <span>Birth: ${date_of_birth ? date_of_birth : `Not found`} </span>
+                    </p>
+                    <p class="flex items-center">
+                        <span class="mr-2"><img src="images/icon/gender.png" class="w-5 h-5" alt=""></span>
+                        <span>Gender: ${gender ? gender : `Not found`} </span>
+                    </p>
+                    <p class="flex items-center">
+                        <span class="mr-2"><img src="images/icon/dollar.png" class="w-5 h-5" alt=""></span>
+                         <span>Price : ${price ? price + '$' : `Not found`}</span>
+                    </p>
+            </div>
+            <div class="border border-solid border-[primary]/[0.1] my-4"></div>
+            <div class="flex justify-between items-center">
+                <button
+                        class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick= showImage('${image}')><i
+                        class="fa-regular fa-thumbs-up"></i>
+                </button>
+                <button
+                    class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick = "showAdoptionModal(this)">Adopt
+                </button>
+                <button
+                    class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" data-title = "${pet_details}" onclick = "showModal(this,'${breed ? breed : ``}','${date_of_birth ? date_of_birth : ``}','${gender ? gender : ``}','${image}','${pet_name}','${price ? price : ``}','${vaccinated_status ? vaccinated_status : ``}')">Details
+                </button>
+            </div>
+        </div>
             `
-        <div class="p-5 rounded-xl border border-solid border-[primary]/[0.1]">
-            <div class="mb-6"><img src="${image} " class="w-full rounded-lg h-[300px] md:h-[330px] lg:h-[360px] object-cover" alt=""></div>
-            <h1 class="text-base md:text-lg lg:text-xl font-bold text-primary mb-4">${pet_name} </h1>
-            <div class="text-sm md:text-sm lg:text-base font-normal text-primary/[0.7] mb-4 space-y-1">
-                <p class="flex items-center">
-                    <span class="mr-2"><img src="images/icon/square.png" class="w-5 h-5" alt=""></span>
-                    <span>Breed: ${breed ? breed : `Not found`} </span>
-                </p>
-                <p class="flex items-center">
-                    <span class="mr-2"><img src="images/icon/calender.png" class="w-5 h-5"  alt=""></span>
-                    <span>Birth: ${date_of_birth ? date_of_birth : `Not found`} </span>
-                </p>
-                <p class="flex items-center">
-                    <span class="mr-2"><img src="images/icon/gender.png" class="w-5 h-5" alt=""></span>
-                    <span>Gender: ${gender ? gender : `Not found`} </span>
-                </p>
-                <p class="flex items-center">
-                    <span class="mr-2"><img src="images/icon/dollar.png" class="w-5 h-5" alt=""></span>
-                     <span>Price : ${price ? price + '$' : `Not found`}</span>
-                </p>
-        </div>
-        <div class="border border-solid border-[primary]/[0.1] my-4"></div>
-        <div class="flex justify-between items-center">
-            <button
-                    class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick= showImage('${image}')><i
-                    class="fa-regular fa-thumbs-up"></i>
-            </button>
-            <button
-                class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick = "showAdoptionModal(this)">Adopt
-            </button>
-            <button
-                class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" data-title = "${pet_details}" onclick = "showModal(this,'${breed ? breed : ``}','${date_of_birth ? date_of_birth : ``}','${gender ? gender : ``}','${image}','${pet_name}','${price ? price : ``}','${vaccinated_status ? vaccinated_status : ``}')">Details
-            </button>
-        </div>
-    </div>
-        `
 
-    })
+        })
+    }, 2000)
 }
 
 // animal card section right div 
@@ -197,8 +215,8 @@ const showAdoptionModal = (btnElement) => {
             btnElement.setAttribute("disabled", true)
             btnElement.innerText = 'Adopted'
             console.log(btnElement);
-            
-            
+
+
         }
         else {
             document.getElementById('modal-count').innerText = count;
