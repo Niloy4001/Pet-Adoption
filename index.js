b7d71ce85e04435d923ab92f79f562bfa1120387
Niloy4001 <huzaifasikderniloy58bd@gmail.com>
2024-10-06 17:52:52 +0600

    sort funtioanality added

--- index.js
+++ index.js
@@ -60,17 +60,72 @@ const fetchAllPets = async (btnElement, categoryName) => {
 
 fetchAllPets()
 
-// loading by spinner
-// const loading = () =>{
-//     document.getElementById('spinner').classList.remove('hidden');
-//     document.getElementById('animal-card-section');
-//     setTimeout(()=>{
-//         fetchAllPets()       
 
-//     },5000)
-// }
-// loading();
+
+// sort element
+const sortElement = (array) =>{
+    const cardParentDiv = document.getElementById('animal-card-parent-div')
+    cardParentDiv.innerHTML = '';
+    if (array.length == 0) {
+        document.getElementById('empty-section').classList.remove('hidden');
+
+    } else {
+        document.getElementById('empty-section').classList.add('hidden');
+    }
+    array.forEach((item) => {
+        const { breed, category, date_of_birth, gender, image, petId, pet_details, pet_name, price, vaccinated_status } = item
+        cardParentDiv.innerHTML +=
+            `
+        <div class="p-5 rounded-xl border border-solid border-[primary]/[0.1]">
+            <div class="mb-6"><img src="${image} " class="w-full rounded-lg h-[300px] md:h-[330px] lg:h-[360px] object-cover" alt=""></div>
+            <h1 class="text-base md:text-lg lg:text-xl font-bold text-primary mb-4">${pet_name} </h1>
+            <div class="text-sm md:text-sm lg:text-base font-normal text-primary/[0.7] mb-4 space-y-1">
+                <p class="flex items-center">
+                    <span class="mr-2"><img src="images/icon/square.png" class="w-5 h-5" alt=""></span>
+                    <span>Breed: ${breed ? breed : `Not found`} </span>
+                </p>
+                <p class="flex items-center">
+                    <span class="mr-2"><img src="images/icon/calender.png" class="w-5 h-5"  alt=""></span>
+                    <span>Birth: ${date_of_birth ? date_of_birth : `Not found`} </span>
+                </p>
+                <p class="flex items-center">
+                    <span class="mr-2"><img src="images/icon/gender.png" class="w-5 h-5" alt=""></span>
+                    <span>Gender: ${gender ? gender : `Not found`} </span>
+                </p>
+                <p class="flex items-center">
+                    <span class="mr-2"><img src="images/icon/dollar.png" class="w-5 h-5" alt=""></span>
+                     <span>Price : ${price ? price + '$' : `Not found`}</span>
+                </p>
+        </div>
+        <div class="border border-solid border-[primary]/[0.1] my-4"></div>
+        <div class="flex justify-between items-center">
+            <button
+                    class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick= showImage('${image}')><i
+                    class="fa-regular fa-thumbs-up"></i>
+            </button>
+            <button
+                class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" onclick = "showAdoptionModal(this)">Adopt
+            </button>
+            <button
+                class="btn bg-white text-base md:text-lg lg:text-xl font-bold text-secondary text-center border border-solid border-[secondary] hover:bg-secondary hover:text-white" data-title = "${pet_details}" onclick = "showModal(this,'${breed ? breed : ``}','${date_of_birth ? date_of_birth : ``}','${gender ? gender : ``}','${image}','${pet_name}','${price ? price : ``}','${vaccinated_status ? vaccinated_status : ``}')">Details
+            </button>
+        </div>
+    </div>
+        `
+
+    })
+    console.log(array);
+    
+}
+// show all pets
 const showAllPets = (array) => {
+
+    document.getElementById('sort-btn').addEventListener('click', function () {
+        array.sort((a, b) => b.price - a.price);
+        sortElement(array);
+        
+    })
+
     const cardParentDiv = document.getElementById('animal-card-parent-div')
     cardParentDiv.innerHTML = '';
     document.getElementById('spinner').classList.remove('hidden');
